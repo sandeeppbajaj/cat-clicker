3f7d2d89b4d7f4fc50609052688705b09ce40bc7
sandeeppbajaj <sandeepp.bajaj@gmail.com>
2015-09-12 14:58:12 -0700

    Add a list of cats and let user select the cat from the list

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,34 @@
-var count = [0,0];
-var countElems = [];
+var cats = [
+  {name:'Kat',imageUrl:'images/cat-1.jpg',clickCount:0},
+  {name:'Butter Cup',imageUrl:'images/cat-2.jpg',clickCount:0},
+  {name:'Bubbles',imageUrl:'images/cat-3.jpg',clickCount:0},
+  {name:'Fuzzy',imageUrl:'images/cat-4.jpg',clickCount:0},
+  {name:'Mouse',imageUrl:'images/cat-5.jpg',clickCount:0}
+];
 
-// Setting intial count for all cats
-for(var i=0;i<2;i++){
-  countElems.push(document.getElementById('click-count-'+(i+1)));
-  countElems[i].innerHTML = count[i];
+var catListElem = document.getElementById('cat-list');
+for(var i=0; i<cats.length; i++){
+  var listItem = document.createElement('li');
+  listItem.innerHTML = "<a href='javascript:changeCat("+i+")'>" + cats[i].name + "</a>";
+  listItem.setAttribute('class','cat-list-item');
+  catListElem.appendChild(listItem);
 }
 
-// List of Image elements
-var imageElems = [document.getElementById('cat-img-1'),document.getElementById('cat-img-2')];
+var currentIndex = 0;
+var catNameElem = document.getElementById('cat-name');
+var catImageElem = document.getElementById('cat-img');
+var clickCountElem = document.getElementById('click-count');
 
-// Add Click event listener for image elements using Closure function
-for(var i=0;i<2;i++ ){
-  imageElems[i].addEventListener('click', (function(index){
-      return function(){
-        count[index]++;
-        countElems[index].innerHTML = count[index];
-      }
-  })(i), false);
+function changeCat(index){
+    currentIndex = index;
+    catNameElem.innerHTML = cats[index].name;
+    catImageElem.setAttribute('src',cats[index].imageUrl);
+    clickCountElem.innerHTML = cats[index].clickCount;
 }
+
+function incrementCount(index){
+  cats[currentIndex].clickCount++;
+  clickCountElem.innerHTML = cats[currentIndex].clickCount;
+}
+
+changeCat(0);
