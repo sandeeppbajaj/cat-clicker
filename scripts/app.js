68a9860610748f9b041bff8201416cfe24030acc
sandeeppbajaj <sandeepp.bajaj@gmail.com>
2015-09-12 12:43:29 -0700

    Using closure function to add click event handlers

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,21 @@
 var count = [0,0];
 var countElems = [];
 
+// Setting intial count for all cats
 for(var i=0;i<2;i++){
   countElems.push(document.getElementById('click-count-'+(i+1)));
   countElems[i].innerHTML = count[i];
 }
 
+// List of Image elements
+var imageElems = [document.getElementById('cat-img-1'),document.getElementById('cat-img-2')];
 
-var imageElem1 = document.getElementById('cat-img-1');
-var imageElem2 = document.getElementById('cat-img-2');
-
-imageElem1.addEventListener('click',function(){
-  count[0]++;
-  countElems[0].innerHTML = count[0];
-},false);
-
-imageElem2.addEventListener('click',function(){
-  count[1]++;
-  countElems[1].innerHTML = count[1];
-},false);
+// Add Click event listener for image elements using Closure function
+for(var i=0;i<2;i++ ){
+  imageElems[i].addEventListener('click', (function(index){
+      return function(){
+        count[index]++;
+        countElems[index].innerHTML = count[index];
+      }
+  })(i), false);
+}
